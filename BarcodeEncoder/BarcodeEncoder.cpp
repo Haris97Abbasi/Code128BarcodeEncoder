356c0da1f4b8112a4f223388b74610a3aaec1cab
Haris Abbasi <haris97duisburg@gmail.com>
2026-09-01 10:54:10 +0500

    Main Method

--- BarcodeEncoder/BarcodeEncoder.cpp
+++ BarcodeEncoder/BarcodeEncoder.cpp
@@ -1,20 +1,31 @@
-// BarcodeEncoder.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+#include "Encoder.h"
 #include <iostream>
+#include <vector>
+#include <string>
 
-int main()
+int main(int argc, char** argv)
 {
-    std::cout << "Hello World!\n";
+	if (argc != 3 || std::string(argv[1]) != "--encode") {
+		std::cerr << "Invalid input command";
+		return 1;
+	}
+	/*std::cout << argc << std::endl;
+	std::cout << argv[0] << std::endl;
+	std::cout << argv[1] << std::endl;
+	std::cout << argv[2] << std::endl;*/
+
+	try {
+		barcode::Encoder encoder;
+		std::vector<int> encodedMessage;
+		encodedMessage = encoder.encode(argv[2]);
+		for (int i = 0; i < encodedMessage.size(); i++) {
+			std::cout << encodedMessage[i] << " ";
+		}
+		std::cout << std::endl;
+	}
+	catch (const std::exception& ex){
+		std::cerr <<"An Error has occured: " << ex.what() << std::endl;
+	}
+
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
