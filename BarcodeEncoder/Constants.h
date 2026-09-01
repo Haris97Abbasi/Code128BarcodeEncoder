d877321017c0339433f3b965b223d8435fdc9aad
Haris Abbasi <haris97duisburg@gmail.com>
2026-09-01 08:51:58 +0500

    Constants

---
+++ BarcodeEncoder/Constants.h
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace barcode {
+
+	inline constexpr int STARTCODE = 104;
+	inline constexpr int STOPCODE = 106;
+	inline constexpr int ASCII_MIN = 32;
+	inline constexpr int ASCII_MAX = 127;
+	inline constexpr int ASCII_DIFFERENCE = 32;
+	inline constexpr int MODULUS = 103;
+}
