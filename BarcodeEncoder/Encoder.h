5a73f6b3e768eac5ecec92cd3a2a48a176faee9b
Haris Abbasi <haris97duisburg@gmail.com>
2026-09-01 08:46:17 +0500

    Header file of Encoder

---
+++ BarcodeEncoder/Encoder.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <vector>
+#include <string>
+
+namespace barcode {
+	class Encoder {
+
+	public:
+		// Used to encode the input string to Barcode-128 Subset B; Uses other functions
+		std::vector<int> encode(const std::string& message) const;
+
+		// Used to calculate teh checksum
+		int calculateChecksum(const std::vector<int>& encodedValues);
+
+		// Used to encode a char into ASCII number
+		int encodeCharacter(char ch);
+
+	private:
+		// Used for validation
+		void validate(const std::string& message);
+
+	};
+}
